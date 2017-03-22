b70ad7d74674c3607f5edf7615d14eabca9b4b0d
李鹏飞 <allen_li89@163.com>
2017-03-22 12:54:31 +0800

    init front end

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require('path');
 var router = require('./router');
 var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
 app.use(router);
+app.use(express.static(path.join(__dirname, 'public')))
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
