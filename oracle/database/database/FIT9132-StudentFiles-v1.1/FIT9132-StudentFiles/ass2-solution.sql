c4927b03d0dbb1307e672d2002886249c9cad149
lpf <allen_li89@163.com>
2017-09-18 21:05:50 +0800

    finished with question

---
+++ oracle/database/database/FIT9132-StudentFiles-v1.1/FIT9132-StudentFiles/ass2-solution.sql
@@ -0,0 +1,112 @@
+/*
+    FIT9132 2017 Semester 2 Assignment 2 SOLUTIONS SCRIPT
+    
+    Student Name:
+    
+    Student ID:
+    
+	Studio Class:
+	
+	Tutor:
+	
+	Comments for your marker:
+	
+	
+
+*/
+
+
+-- Task 1 Data Definition
+-- ======================
+
+-- Task 1.1
+
+
+
+    
+-- Task 1.2
+-- drop table statements
+-- here you must NOT use CASCADE constraints
+
+
+
+
+-- Task 2 Data Manipulation
+-- ========================
+
+-- Task 2.1
+-- Add to your database four DINER records and their associated FS_DINER records
+
+
+
+
+-- Task 2.2
+-- Provide the create sequence commands to be used for primary key vales when adding 
+-- food items and diners to the system. 
+-- - the food item sequence should start at 11 and increment by 1
+-- - the diner sequence should start at 10 and increment by 1
+
+
+
+
+-- Task 2.3    
+--  Provide the drop sequence statements for the two sequences you created in  Q2.1 
+
+
+
+
+-- Task 3 Database Insert/Updates
+-- ==============================
+
+-- SEQUENCES created in task 2.2 must be used in this task for the adding primary keys.
+
+-- Task 3.1
+--  Add a new DESSERT to the Monash food menu - you will need to research some
+-- meaningful data to be able to add this item.  DESSERT's are food_type 'D' and are 
+-- only served in standard 'ST' serve sizes.
+
+
+
+
+-- Task 3.2
+-- Monash food has decided to increase the price charged for all standard serve  
+-- ('ST') main food items ('M' food type) by 15%, make this change in the database
+
+
+
+
+-- Task 3.3 Diner Activity
+
+-- Task 3.3 (a) A new diner has just arrived and been seated at Table 1 seat 3. Update the  
+-- database to seat this diner
+
+
+
+
+-- Task 3.3 (b) This new diner calls the waiter over and proceeds to order two 'Bruschetta' 
+-- entrees. Entrees are only available in a standard 'ST' size. Add this data to the 
+-- Monash Food System for this diner. The food item has not been served as yet, this is 
+-- an order only
+
+
+
+
+-- Task 3.3 (c) Some time after this order has been recorded the 'Bruschetta' are served to 
+-- this diner - update the database to record this service. 
+
+
+
+
+-- Task 4 Database Structure
+-- =========================
+-- Task 4.1 Collection of Diner information
+
+
+
+
+-- Task 4.2 End of financial year DINER and FS_DINER archive
+
+
+
+
+--========================= END OF ASS2-SOLUTION.SQL ==================================
