4da3f095d33674f9e2c835585f3a9f1f6f57577c
lpf <allen_li89@163.com>
2017-09-24 14:54:54 +0800

    finshed square

--- java/Square.java
+++ java/Square.java
@@ -1,8 +1,16 @@
 /**
-*
+* FIlE: Square.java
+* AUTHOR:;
+* DATE: 2017-09-24
+* DESC: Implement a class Sqaure to represent a square:
+*       it has a attribute width, it offer methods to calculate the area and perimeter
 **/
 public class Square{
     private int width;
+
+    /*
+    constructor for class
+    */
     public Square(int w){
         width = w;
     }
@@ -11,18 +19,28 @@ public class Square{
         width = w;
     }
 
+
     public int getWidth(){
         return with;
     }
 
+    /*
+    return the area of the square
+    */
     public int area(){
         return width*width;
     }
 
+    /*
+    return the perimeter of the sqaure
+    */
     public int perimeter(){
         return width * 4;
     }
 
+    /*
+    print instance of the class informatively
+    */
     public String toString(){
         return "<Sqaure width=" + width + ">";
     }
